ce524f680dd0244b7e3013d937c10c32f4c21b5d
cnmorocho <carlos.nahuel.morocho@gmail.com>
2025-03-06 23:57:35 -0300

    se agrego formatter de go

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,24 +1,15 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  -- This will provide type hinting with LuaLS
-  ---@module "conform"
-  ---@type conform.setupOpts
   opts = {
-    -- Define your formatters
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "isort", "black" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      go = { "gofumpt", "golines" },
     },
-    -- Set default options
     default_format_opts = {
       lsp_format = "fallback",
     },
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
-    -- Customize formatters
     formatters = {
       shfmt = {
         prepend_args = { "-i", "2" },
